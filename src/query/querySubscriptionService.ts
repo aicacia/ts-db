5ffbe974e896e877159f123c5db199847524860f
Nathan Faucett <nathanfaucett@gmail.com>
2026-04-16 20:26:41 -0400

    refactor: update QuerySubscriptionService implementation for improved configuration handling

--- src/query/querySubscriptionService.ts
+++ src/query/querySubscriptionService.ts
@@ -1,11 +1,10 @@
 import type { SourceAdapter, UnsubscribeFn } from "../types/index.js";
 import { getCTEIdentity } from "./cte.js";
 import type { CTE } from "./cte.js";
-import { toError } from "../utils/index.js";
 import {
 	createSubscriptionManager,
-	type SubscriptionManager,
 	type SubscriptionAdapter,
+	type SubscriptionManager,
 } from "./subscriptionManager.js";
 import type { SourceSubscription } from "./sourceSubscription.js";
 import { createSourceSubscription } from "./sourceSubscription.js";
@@ -23,7 +22,7 @@ export type { QueryExecutionPort } from "./queryExecution.js";
 
 export interface QuerySubscriptionServiceConfig<T> {
 	source?: SourceAdapter<T, CTE<T>>;
-	sourceSubscription?: SubscriptionAdapter<T, CTE<T>>;
+	sourceSubscription?: SourceSubscription<T, CTE<T>>;
 	subscriptionManager?: SubscriptionManager<T>;
 	queryExecutor?: QueryExecutionPort<T>;
 	keySerializer?: (cte: CTE<T>) => string;
@@ -42,79 +41,82 @@ export interface QuerySubscriptionService<T> {
 	fetchSnapshot(cte: CTE<T>): T[];
 }
 
-export function createQuerySubscriptionService<T>(
-	config: QuerySubscriptionServiceConfig<T>,
-): QuerySubscriptionService<T> {
-	if (!config.source && !config.sourceSubscription) {
-		throw new Error(
-			"QuerySubscriptionService requires either source or sourceSubscription",
-		);
+class QuerySubscriptionServiceImpl<T> implements QuerySubscriptionService<T> {
+	private readonly sourceSubscription: SourceSubscription<T, CTE<T>>;
+	private readonly subscriptionManager: SubscriptionManager<T>;
+	private readonly queryExecutor: QueryExecutionPort<T>;
+	private readonly serializeKey: (cte: CTE<T>) => string;
+
+	constructor(config: QuerySubscriptionServiceConfig<T>) {
+		if (!config.source && !config.sourceSubscription) {
+			throw new Error(
+				"QuerySubscriptionService requires either source or sourceSubscription",
+			);
+		}
+
+		this.sourceSubscription =
+			config.sourceSubscription ??
+			createSourceSubscription(config.source as SourceAdapter<T, CTE<T>>);
+		this.subscriptionManager =
+			config.subscriptionManager ?? createSubscriptionManager<T>();
+		this.queryExecutor =
+			config.queryExecutor ?? createDefaultQueryExecutionPort<T>();
+		this.serializeKey = config.keySerializer ?? getCTEIdentity;
 	}
 
-	let sourceSubscription: SubscriptionAdapter<T, CTE<T>>;
-	if (config.sourceSubscription) {
-		sourceSubscription = config.sourceSubscription;
-	} else if (config.source) {
-		sourceSubscription = createSourceSubscription(config.source);
-	} else {
-		throw new Error(
-			"QuerySubscriptionService requires either source or sourceSubscription",
-		);
+	subscribe(
+		cte: CTE<T>,
+		callbacks: QuerySubscriptionCallback<T>,
+	): UnsubscribeFn {
+		return this.createSubscription(cte)(callbacks);
 	}
-	const subscriptionManager =
-		config.subscriptionManager ?? createSubscriptionManager<T>();
-	const queryExecutor: QueryExecutionPort<T> =
-		config.queryExecutor ?? createDefaultQueryExecutionPort<T>();
-	const serializeKey = config.keySerializer ?? getCTEIdentity;
 
-	const createQuerySubscription = (cte: CTE<T>): QuerySubscriptionResult<T> => {
+	createSubscription(cte: CTE<T>): QuerySubscriptionResult<T> {
+		const subscriptionKey = this.serializeKey(cte);
+
+		const adapterFactory = (): SubscriptionAdapter<T> =>
+			createQuerySubscriptionAdapter(cte, this.sourceSubscription, this.queryExecutor);
+
 		return (callbacks) => {
-			const subscriptionKey = serializeKey(cte);
-
-			const adapterFactory = () =>
-				createQuerySubscriptionAdapter(cte, sourceSubscription, queryExecutor);
-
-			return subscriptionManager.subscribe({
+			return this.subscriptionManager.subscribe({
 				id: subscriptionKey,
 				adapterFactory,
 				onUpdate: callbacks.onUpdate,
 				onError: callbacks.onError,
 			});
 		};
-	};
+	}
 
-	const createQueryBuilderForService = (): IQueryBuilder<T> => {
-		return createQueryBuilder((cte: CTE<T>) => createQuerySubscription(cte));
-	};
+	createQueryBuilder(): IQueryBuilder<T> {
+		return createQueryBuilder((cte: CTE<T>) => this.createSubscription(cte));
+	}
 
-	return {
-		subscribe(cte, callbacks) {
-			return createQuerySubscription(cte)(callbacks);
-		},
+	fetchSnapshot(cte: CTE<T>): T[] {
+		const subscriptionKey = this.serializeKey(cte);
+		const snapshot = this.subscriptionManager.getSnapshot(subscriptionKey);
+		if (snapshot !== null) {
+			return snapshot;
+		}
 
-		createSubscription: createQuerySubscription,
-		createQueryBuilder: createQueryBuilderForService,
+		const snapshotProvider = this.sourceSubscription.getSnapshot;
+		if (!snapshotProvider) {
+			throw new Error(
+				"Source subscription does not support snapshot retrieval",
+			);
+		}
 
-		fetchSnapshot(cte: CTE<T>) {
-			const subscriptionKey = serializeKey(cte);
-			const snapshot = subscriptionManager.getSnapshot(subscriptionKey);
-			if (snapshot !== null) {
-				return snapshot;
-			}
+		const sourceDocs = snapshotProvider.call(this.sourceSubscription);
 
-			const snapshotProvider = sourceSubscription.getSnapshot;
-			if (!snapshotProvider) {
-				throw new Error(
-					"Source subscription does not support snapshot retrieval",
-				);
-			}
-
-			const sourceDocs = snapshotProvider.call(sourceSubscription, cte);
-
-			return queryExecutor.execute(cte, [...sourceDocs]);
-		},
-	};
+		return this.queryExecutor.execute(cte, [...sourceDocs]);
+	}
 }
+
+export function createQuerySubscriptionService<T>(
+	config: QuerySubscriptionServiceConfig<T>,
+): QuerySubscriptionService<T> {
+	return new QuerySubscriptionServiceImpl(config);
+}
+
 export function createQueryService<T>(
 	config: QueryServiceConfig<T>,
 ): QueryService<T> {
